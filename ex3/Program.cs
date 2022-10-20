e9d21cb2cefc5447350bafe9afad2647191a5e0a
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-20 20:01:00 +0300

    ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("number");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(number);
+int a = 1;
+int b = 1;
+
+while (a <= number)
+{
+b = a * b;
+
+Console.WriteLine(b);
+a = a+1;
+}
