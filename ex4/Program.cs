34b6ecdc3da32c7bf1f8633ef9574150d6581126
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-20 20:16:26 +0300

    ex4

---
+++ ex4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+int number = new Random() .Next();
+Console.WriteLine(number);
+int a = 0;
+a = number * number * number;
+if (a %2==0)
+{
+    Console.WriteLine("yes");
+}
+if (a %2!=0)
+{
+    Console.WriteLine("no");
+}
+
